04a0b76da60a9c243b769eab6abf01d2a7d24467
dingtao <tao.ding92@gmail.com>
2025-12-17 20:22:14 +0800

    修改rknn版本为1.5.2

--- examples/pose_landmarker/android/pose-detector-lib/src/main/cpp/include/rknn_api.h
+++ examples/pose_landmarker/android/pose-detector-lib/src/main/cpp/include/rknn_api.h
@@ -59,41 +59,11 @@ extern "C" {
 /* dummy init flag: could only get total_weight_size and total_internal_size by rknn_query*/
 #define RKNN_FLAG_COLLECT_MODEL_INFO_ONLY       0x00000100
 
-/* allocate internal memory in outside */
-#define RKNN_FLAG_INTERNAL_ALLOC_OUTSIDE        0x00000200
-
 /* set GPU as the preferred execution backend When the operator is not supported by the NPU */
 #define RKNN_FLAG_EXECUTE_FALLBACK_PRIOR_DEVICE_GPU 0x00000400
 
-/* enable allocate sram type buffers */
-#define RKNN_FLAG_ENABLE_SRAM                   0x00000800
-
-/* sram type buffers are shared among different contexts */
-#define RKNN_FLAG_SHARE_SRAM                    0x00001000
-
-/* default nice -19, this flag can disable default priority */
-#define RKNN_FLAG_DISABLE_PROC_HIGH_PRIORITY    0x00002000
-
-/* don't flush input buffer cache, the user must ensure that the input tensor has flushed the cache before calling rknn_run. 
-!!! Don't use this flags when you call rknn_inputs_set() to set input data. */
-#define RKNN_FLAG_DISABLE_FLUSH_INPUT_MEM_CACHE    0x00004000
-
-/* Don't invalid output buffer cache. 
-   Users cannot directly access output_mem->virt_addr, 
-   which will cause cache consistency problems. 
-   If you want to use output_mem->virt_addr, 
-   you must use rknn_mem_sync (ctx, mem, RKNN_MEMORY_SYNC_FROM_DEVICE) to flush the cache. 
-   This flags is generally used when the output data of the NPU is not accessed by the CPU, 
-   but is accessed by the GPU or RGA to reduce the time required to flush the cache. 
-   !!! Don't use this flags when you call rknn_outputs_get() to get output data.*/
-#define RKNN_FLAG_DISABLE_FLUSH_OUTPUT_MEM_CACHE   0x00008000
-
-/* This flag is used when the model data buffer is allocated by NPU, and can be accessed by NPU directly. */
-#define RKNN_FLAG_MODEL_BUFFER_ZERO_COPY           0x00010000
-
-/* This flag is a memory allocation flag, which is used in rknn_create_mem2() when no context is available. */
-#define RKNN_MEM_FLAG_ALLOC_NO_CONTEXT             0x00020000
-
+/* allocate internal memory in outside */
+#define RKNN_FLAG_INTERNAL_ALLOC_OUTSIDE        0x00000200
 
 /*
     Error code returned by the RKNN API.
@@ -182,8 +152,6 @@ typedef enum _rknn_tensor_type {
     RKNN_TENSOR_UINT32,                                 /* data type is uint32. */
     RKNN_TENSOR_INT64,                                  /* data type is int64. */
     RKNN_TENSOR_BOOL,
-    RKNN_TENSOR_INT4,
-    RKNN_TENSOR_BFLOAT16,
 
     RKNN_TENSOR_TYPE_MAX
 } rknn_tensor_type;
@@ -201,8 +169,6 @@ inline static const char* get_type_string(rknn_tensor_type type)
     case RKNN_TENSOR_UINT32: return "UINT32";
     case RKNN_TENSOR_INT64: return "INT64";
     case RKNN_TENSOR_BOOL: return "BOOL";
-    case RKNN_TENSOR_INT4: return "INT4";
-    case RKNN_TENSOR_BFLOAT16: return "BF16";
     default: return "UNKNOW";
     }
 }
@@ -250,7 +216,6 @@ typedef enum _rknn_core_mask {
     RKNN_NPU_CORE_2 = 4,                                          /* run on NPU core 2. */
     RKNN_NPU_CORE_0_1 = RKNN_NPU_CORE_0 | RKNN_NPU_CORE_1,        /* run on NPU core 0 and core 1. */
     RKNN_NPU_CORE_0_1_2 = RKNN_NPU_CORE_0_1 | RKNN_NPU_CORE_2,    /* run on NPU core 0 and core 1 and core 2. */
-    RKNN_NPU_CORE_ALL = 0xffff,                                   /* auto choice, run on NPU cores depending on platform */
 
     RKNN_NPU_CORE_UNDEFINED,
 } rknn_core_mask;
@@ -376,26 +341,6 @@ typedef enum _rknn_tensor_mem_flags {
 } rknn_tensor_mem_flags;
 
 /*
-   The mode to allocate rknn memory.
-*/
-typedef enum _rknn_mem_alloc_flags {
-    RKNN_FLAG_MEMORY_FLAGS_DEFAULT = 0 << 0, /* Same with RKNN_FLAG_MEMORY_CACHEABLE */
-    RKNN_FLAG_MEMORY_CACHEABLE  = 1 << 0, /* Create Cacheable memory. */
-    RKNN_FLAG_MEMORY_NON_CACHEABLE = 1 << 1, /* Create NON-Cacheable memory. */
-    RKNN_FLAG_MEMORY_TRY_ALLOC_SRAM = 1 << 2, /* Try to allocate memory in SRAM if possible. if SRAM is not enough, allocate rest memory in DRAM. */
-} rknn_mem_alloc_flags;
-
-/*
-   The mode to sync cacheable rknn memory.
-*/
-typedef enum _rknn_mem_sync_mode {
-    RKNN_MEMORY_SYNC_TO_DEVICE = 0x1, /* the mode used for consistency of device access after CPU accesses data. */
-    RKNN_MEMORY_SYNC_FROM_DEVICE = 0x2, /* the mode used for consistency of CPU access after device accesses data. */
-    RKNN_MEMORY_SYNC_BIDIRECTIONAL = RKNN_MEMORY_SYNC_TO_DEVICE | RKNN_MEMORY_SYNC_FROM_DEVICE, /* the mode used for consistency of data access
-                                                                                                         between device and CPU in both directions. */
-} rknn_mem_sync_mode;
-
-/*
     the memory information of tensor.
 */
 typedef struct _rknn_tensor_memory {
@@ -447,11 +392,9 @@ typedef struct _rknn_output {
 */
 typedef struct _rknn_init_extend {
     rknn_context ctx;                                    /* rknn context */
-    int32_t      real_model_offset;                      /* real rknn model file offset, only valid when init context with rknn file path and zero-copy model model */
-    uint32_t     real_model_size;                        /* real rknn model file size, only valid when init context with rknn file path and zero-copy model model */
-    int32_t      model_buffer_fd;                        /* the fd of model buffer. */
-    uint32_t     model_buffer_flags;                     /* the flags of model_buffer */
-    uint8_t      reserved[112];                          /* reserved */
+    int32_t      real_model_offset;                      /* real rknn model file offset, only valid when init context with rknn file path */
+    uint32_t     real_model_size;                        /* real rknn model file size, only valid when init context with rknn file path */
+    uint8_t      reserved[120];                          /* reserved */
 } rknn_init_extend;
 
 /*
@@ -557,7 +500,7 @@ int rknn_set_batch_core_num(rknn_context context, int core_num);
 
 /*  rknn_set_core_mask
 
-    set the core mask for the model.(only supported on multi-core NPU platform)
+    set rknn core mask.(only supported on RK3588 now)
 
     RKNN_NPU_CORE_AUTO: auto mode, default value
     RKNN_NPU_CORE_0: core 0 mode
@@ -565,8 +508,6 @@ int rknn_set_batch_core_num(rknn_context context, int core_num);
     RKNN_NPU_CORE_2: core 2 mode
     RKNN_NPU_CORE_0_1: combine core 0/1 mode
     RKNN_NPU_CORE_0_1_2: combine core 0/1/2 mode
-    RKNN_NPU_CORE_ALL: auto mode, select multiple npu cores to run depending on platform 
-
 
     input:
         rknn_context context        the handle of context.
@@ -694,18 +635,6 @@ rknn_tensor_mem* rknn_create_mem_from_mb_blk(rknn_context ctx, void *mb_blk, int
 */
 rknn_tensor_mem* rknn_create_mem(rknn_context ctx, uint32_t size);
 
-/*  rknn_create_mem2 (memory allocated inside)
-
-    create tensor memory.
-
-    input:
-        rknn_context ctx            the handle of context.
-        uint64_t size               the size of tensor buffer.
-        uint64_t alloc_flags        memory allocation flags.
-    return:
-        rknn_tensor_mem             the pointer of tensor memory information.
-*/
-rknn_tensor_mem* rknn_create_mem2(rknn_context ctx, uint64_t size, uint64_t alloc_flags);
 
 /*  rknn_destroy_mem (support allocate inside and outside)
 
@@ -783,19 +712,6 @@ int rknn_set_input_shape(rknn_context ctx, rknn_tensor_attr* attr);
         int                         error code.
 */
 int rknn_set_input_shapes(rknn_context ctx, uint32_t n_inputs, rknn_tensor_attr attr[]);
-
-/*  rknn_mem_sync
-
-    sync cacheable rknn memory when both cpu and device access data.
-
-    input:
-        rknn_context context        the handle of context.
-        rknn_tensor_mem *mem        the pointer of tensor memory information.
-        rknn_mem_sync_mode mode     the mode of sync cache.
-    return:
-        int                         error code.
-*/
-int rknn_mem_sync(rknn_context context, rknn_tensor_mem* mem, rknn_mem_sync_mode mode);
 
 #ifdef __cplusplus
 } //extern "C"
--- examples/pose_landmarker/android/pose-detector-lib/src/main/cpp/include/rknn_matmul_api.h
+++ examples/pose_landmarker/android/pose-detector-lib/src/main/cpp/include/rknn_matmul_api.h
@@ -21,79 +21,6 @@ extern "C" {
 
 typedef rknn_context rknn_matmul_ctx;
 
-typedef enum _rknn_matmul_quant_type
-{
-  RKNN_QUANT_TYPE_PER_LAYER_SYM    = 0,
-  RKNN_QUANT_TYPE_PER_LAYER_ASYM   = 1,
-  RKNN_QUANT_TYPE_PER_CHANNEL_SYM  = 2,
-  RKNN_QUANT_TYPE_PER_CHANNEL_ASYM = 3,
-  RKNN_QUANT_TYPE_PER_GROUP_SYM    = 4,
-  RKNN_QUANT_TYPE_PER_GROUP_ASYM   = 5,
-} rknn_matmul_quant_type;
-
-typedef struct _rknn_quant_params
-{
-  char name[RKNN_MAX_NAME_LEN];
-
-  // matmul tensor scale
-  float*  scale;
-  int32_t scale_len;
-
-  // matmul tensor zero point
-  int32_t* zp;
-  int32_t  zp_len;
-
-} rknn_quant_params;
-
-typedef enum _rknn_matmul_type
-{
-  RKNN_FLOAT16_MM_FLOAT16_TO_FLOAT32 = 1,
-  RKNN_INT8_MM_INT8_TO_INT32         = 2,
-  RKNN_INT8_MM_INT8_TO_INT8          = 3,
-  RKNN_FLOAT16_MM_FLOAT16_TO_FLOAT16 = 4,
-  RKNN_FLOAT16_MM_INT8_TO_FLOAT32    = 5,
-  RKNN_FLOAT16_MM_INT8_TO_FLOAT16    = 6,
-  RKNN_FLOAT16_MM_INT4_TO_FLOAT32    = 7,
-  RKNN_FLOAT16_MM_INT4_TO_FLOAT16    = 8,
-  RKNN_INT8_MM_INT8_TO_FLOAT32       = 9,
-  RKNN_INT4_MM_INT4_TO_INT16         = 10,
-  RKNN_INT8_MM_INT4_TO_INT32         = 11,
-  RKNN_FLOAT16_MM_INT4_TO_BFLOAT16   = 12,
-  RKNN_INT8_MM_INT4_TO_FLOAT16       = 15,
-} rknn_matmul_type;
-
-inline static const char* get_matmul_type_string(rknn_matmul_type type)
-{
-  switch (type) {
-  case RKNN_FLOAT16_MM_FLOAT16_TO_FLOAT32:
-    return "RKNN_FLOAT16_MM_FLOAT16_TO_FLOAT32";
-  case RKNN_INT8_MM_INT8_TO_INT32:
-    return "RKNN_INT8_MM_INT8_TO_INT32";
-  case RKNN_INT8_MM_INT8_TO_INT8:
-    return "RKNN_INT8_MM_INT8_TO_INT8";
-  case RKNN_FLOAT16_MM_FLOAT16_TO_FLOAT16:
-    return "RKNN_FLOAT16_MM_FLOAT16_TO_FLOAT16";
-  case RKNN_FLOAT16_MM_INT8_TO_FLOAT32:
-    return "RKNN_FLOAT16_MM_INT8_TO_FLOAT32";
-  case RKNN_FLOAT16_MM_INT8_TO_FLOAT16:
-    return "RKNN_FLOAT16_MM_INT8_TO_FLOAT16";
-  case RKNN_INT4_MM_INT4_TO_INT16:
-    return "RKNN_INT4_MM_INT4_TO_INT16";
-  case RKNN_FLOAT16_MM_INT4_TO_FLOAT32:
-    return "RKNN_FLOAT16_MM_INT4_TO_FLOAT32";
-  case RKNN_FLOAT16_MM_INT4_TO_FLOAT16:
-    return "RKNN_FLOAT16_MM_INT4_TO_FLOAT16";
-  case RKNN_INT8_MM_INT4_TO_INT32:
-    return "RKNN_INT8_MM_INT4_TO_INT32";
-  case RKNN_INT8_MM_INT8_TO_FLOAT32:
-    return "RKNN_INT8_MM_INT8_TO_FLOAT32";
-  case RKNN_FLOAT16_MM_INT4_TO_BFLOAT16:
-    return "RKNN_FLOAT16_MM_INT4_TO_BFLOAT16";
-  default:
-    return "UNKNOW";
-  }
-}
-
 typedef struct _rknn_matmul_tensor_attr
 {
   char name[RKNN_MAX_NAME_LEN];
@@ -109,7 +36,6 @@ typedef struct _rknn_matmul_tensor_attr
   // int8 : A, B
   // int32: C
   rknn_tensor_type type;
-
 } rknn_matmul_tensor_attr;
 
 typedef struct _rknn_matmul_io_attr
@@ -121,73 +47,27 @@ typedef struct _rknn_matmul_io_attr
 } rknn_matmul_io_attr;
 
 /*
-  matmul dynamic shape struct
-*/
-typedef struct _rknn_matmul_shape
-{
-  int32_t M;
-  int32_t K;
-  int32_t N;
-} rknn_matmul_shape;
-
-/*
-  the layout of matmul input/output tensor.
-*/
-typedef enum
-{
-  RKNN_MM_LAYOUT_NORM    = 0,
-  RKNN_MM_LAYOUT_NATIVE  = 1,
-  RKNN_MM_LAYOUT_TP_NORM = 2,
-} rknn_matmul_layout;
-
-/*
   matmul information struct
  */
 typedef struct rknn_matmul_info_t
 {
   int32_t M;
-  int32_t K; // limit: RK3566/3568: int8 type must be aligned with 32byte, float16 type must be aligned with 16byte;
-             // RK3562:      int8 type must be aligned with 32byte, float16 type must be aligned with 32byte;
-             // RK3588/3576: int8 type must be aligned with 32byte, float16 type must be aligned with 32byte,
-             //              int4 type must be aligned with 32byte;
-  int32_t N; // limit: RK3566/3568: int8 type must be aligned with 16byte, float16 type must be aligned with 8byte;
-             // RK3562:      int8 type must be aligned with 16byte, float16 type must be aligned with 8byte;
-             // RK3588/3576: int8 type must be aligned with 32byte, float16 type must be aligned with 16byte,
-             //              int4 type must be aligned with 64byte;
-  // matmul data type
-  // int4: int4(A) x int4(B) -> int16(C)
+  int32_t K; // limit: must be aligned with 32byte
+  int32_t N; // limit: must be aligned with 16byte
+
+  // matmul data type, only support int8 currently
   // int8: int8(A) x int8(B) -> int32(C)
-  // float16: float16(A) x float16(B) -> float32(C)
-  rknn_matmul_type type;
+  rknn_tensor_type type;
 
   // matmul native layout for B
   // 0: normal layout
   // 1: native layout
-  int16_t B_layout;
+  int32_t native_layout;
 
-  // matmul quant type for B
-  // A and C only support per layer
-  // 0: per layer
-  // 1: per channel
-  // 2: per group
-  int16_t B_quant_type;
-
-  // matmul native layout for A and C
+  // matmul perf layout for A and C
   // 0: normal layout
-  // 1: native layout
-  int16_t AC_layout;
-
-  // matmul quant type for A and C, only support 0
-  int16_t AC_quant_type;
-
-  // iommu domain id, each domain has 4GB of space
-  int32_t iommu_domain_id;
-
-  // B_quant_type set 2, group size is enable
-  int16_t group_size;
-
-  // reserved field
-  int8_t reserved[34];
+  // 1: perf layout
+  int32_t perf_layout;
 } rknn_matmul_info;
 
 /*  rknn_matmul_create
@@ -200,23 +80,6 @@ typedef struct rknn_matmul_info_t
         int                         error code
 */
 int rknn_matmul_create(rknn_matmul_ctx* ctx, rknn_matmul_info* info, rknn_matmul_io_attr* io_attr);
-
-/*  rknn_matmul_create_dynamic_shape
-
-    params:
-        rknn_matmul_ctx *ctx                the handle of context.
-        rknn_matmul_info *info              the matmal information.
-        int shape_num                       the supported shape number of matmul.
-        rknn_matmul_shape dynamic_shapes[]  the supported M,K,N shape struct array.
-        rknn_matmul_io_attr *io_attr        the array of inputs and output attribute
-    return:
-        int                                 error code
-*/
-/*
-  原来的info.M, K, N无效
-*/
-int rknn_matmul_create_dynamic_shape(rknn_matmul_ctx* ctx, rknn_matmul_info* info, int shape_num,
-                                 rknn_matmul_shape dynamic_shapes[], rknn_matmul_io_attr io_attrs[]);
 
 /* rknn_matmul_set_io_mem
 
@@ -231,62 +94,17 @@ int rknn_matmul_create_dynamic_shape(rknn_matmul_ctx* ctx, rknn_matmul_info* inf
       C = A * B,
 
     limit:
-      K max:   k <= 10240
-      K limit: RK3566/3568: int8 type must be aligned with 32byte, float16 type must be aligned with 16byte;
-               RK3562:      int8 type must be aligned with 32byte, float16 type must be aligned with 32byte;
-               RK3588/3576: int8 type must be aligned with 32byte, float16 type must be aligned with 32byte,
-                            int4 type must be aligned with 32byte;
-      N limit: RK3566/3568: int8 type must be aligned with 16byte, float16 type must be aligned with 8byte;
-               RK3562:      int8 type must be aligned with 16byte, float16 type must be aligned with 8byte;
-               RK3588/3576: int8 type must be aligned with 32byte, float16 type must be aligned with 16byte,
-                            int4 type must be aligned with 64byte;
+      K <= 4096
+      K align with 32bytes
+      N align with 16bytes
+
     A shape: M x K
       normal layout: (M, K)
               [M1K1, M1K2, ..., M1Kk,
                M2K1, M2K2, ..., M2Kk,
                ...
                MmK1, MmK2, ..., MmKk]
-      for RK3566/3568：
-      int8:
-      native layout: (K / 8, M, 8)
-              [K1M1, K2M1,  ..., K8M1,
-               K9M2, K10M2, ..., K16M2,
-               ...
-               K(k-7)Mm, K(k-6)Mm, ..., KkMm]
-      float16:
-      native layout: (K / 4, M, 4)
-              [K1M1, K2M1,  ..., K4M1,
-               K9M2, K10M2, ..., K8M2,
-               ...
-               K(k-3)Mm, K(k-2)Mm, ..., KkMm]
-      for RK3562：
-      int8:
-      native layout: (K / 16, M, 16)
-              [K1M1, K2M1,  ..., K16M1,
-               K17M2, K18M2, ..., K32M2,
-               ...
-               K(k-15)Mm, K(k-14)Mm, ..., KkMm]
-      float16:
-      native layout: (K / 8, M, 8)
-              [K1M1, K2M1,  ..., K8M1,
-               K9M2, K10M2, ..., K16M2,
-               ...
-               K(k-7)Mm, K(k-6)Mm, ..., KkMm]
-      for RK3588/3576：
-      int4:
-      native layout: (K / 32, M, 32)
-              [K1M1, K2M1,  ..., K32M1,
-               K33M2, K10M2, ..., K64M2,
-               ...
-               K(k-31)Mm, K(k-30)Mm, ..., KkMm]
-      int8:
-      native layout: (K / 16, M, 16)
-              [K1M1, K2M1,  ..., K16M1,
-               K17M2, K18M2, ..., K32M2,
-               ...
-               K(k-15)Mm, K(k-14)Mm, ..., KkMm]
-      float16:
-      native layout: (K / 8, M, 8)
+      perf layout: (K / 8, M, 8)
               [K1M1, K2M1,  ..., K8M1,
                K9M2, K10M2, ..., K16M2,
                ...
@@ -297,118 +115,6 @@ int rknn_matmul_create_dynamic_shape(rknn_matmul_ctx* ctx, rknn_matmul_info* inf
                K2N1, K2N2, ..., K2Nn,
                ...
                KkN1, KkN2, ..., KkNn]
-      for RK3566/3568：
-      int8:
-      native layout: (N / 16, K / 32, 16, 32)
-              [K1N1,  K2N1,  ..., K32N1,
-               K1N2,  K2N2,  ..., K32N2,
-               ...
-               K1N16, K2N16, ..., K32N16,
-               K33N1, K34N1, ..., K64N1,
-               K33N2, K34N2, ..., K64N2,
-               ...
-               K(k-31)N16, K(k-30)N16, ..., KkN16,
-               K1N17, K2N17, ..., K32N17,
-               K1N18, K2N18, ..., K32N18,
-               ...
-               K(k-31)Nn, K(k-30)Nn, ..., KkNn]
-      float16:
-      native layout: (N / 8, K / 16, 8, 16)
-              [K1N1,  K2N1,  ..., K16N1,
-               K1N2,  K2N2,  ..., K16N2,
-               ...
-               K1N8,  K2N8,  ..., K16N8,
-               K17N1, K18N1, ..., K32N1,
-               K17N2, K18N2, ..., K32N2,
-               ...
-               K(k-15)N8, K(k-30)N8, ..., KkN8,
-               K1N9,  K2N9,  ..., K16N9,
-               K1N10, K2N10, ..., K16N10,
-               ...
-               K(k-15)Nn, K(k-14)Nn, ..., KkNn]
-      for RK3562：
-      int8:
-      native layout: (N / 16, K / 32, 16, 32)
-              [K1N1,  K2N1,  ..., K32N1,
-               K1N2,  K2N2,  ..., K32N2,
-               ...
-               K1N16, K2N16, ..., K32N16,
-               K33N1, K34N1, ..., K64N1,
-               K33N2, K34N2, ..., K64N2,
-               ...
-               K(k-31)N16, K(k-30)N16, ..., KkN16,
-               K1N17, K2N17, ..., K32N17,
-               K1N18, K2N18, ..., K32N18,
-               ...
-               K(k-31)Nn, K(k-30)Nn, ..., KkNn]
-      float16:
-      native layout: (N / 8, K / 32, 8, 32)
-              [K1N1,  K2N1,  ..., K32N1,
-               K1N2,  K2N2,  ..., K32N2,
-               ...
-               K1N8,  K2N8,  ..., K32N8,
-               K33N1, K34N1, ..., K64N1,
-               K33N2, K34N2, ..., K64N2,
-               ...
-               K(k-31)N8, K(k-30)N8, ..., KkN8,
-               K1N9,  K2N9,  ..., K16N9,
-               K1N10, K2N10, ..., K16N10,
-               ...
-               K(k-31)Nn, K(k-30)Nn, ..., KkNn]
-      for RK3588：
-      when K > 8192, the B data will be split into T segments.
-      int T = std::ceil(K / 8192);
-      For example:  normal layout  -> native layout
-      K =  20488, N = 4096, T = 3, the data will be split into 3 segments.
-      subN = rknn_matmul_io_attr.B.dims[2];
-      subK = rknn_matmul_io_attr.B.dims[3];
-                                      (8196, 4096)          (4096 / subN, 8196 / subK, subN, subK)
-        (K, N) = (20488, 4096)  ->    (8196, 4096)    ->    (4096 / subN, 8196 / subK, subN, subK)
-                 normal layout        (4096, 4096)          (4096 / subN, 4096 / subK, subN, subK)
-                                     T normal layout                    T native layout
-      It is recommended to use the rknn_B_normal_layout_to_native_layout interface for direct data conversion.
-      for RK3576：
-      when K > 4096, the B data will be split into T segments.
-      int T = std::ceil(K / 4096);
-      For example:  normal layout  -> native layout
-      K =  10240, N = 2048, T = 3, the data will be split into 3 segments.
-      subN = rknn_matmul_io_attr.B.dims[2];
-      subK = rknn_matmul_io_attr.B.dims[3];
-                                      (4096, 2048)          (2048 / subN, 4096 / subK, subN, subK)
-        (K, N) = (10240, 2048)  ->    (4096, 2048)    ->    (2048 / subN, 4096 / subK, subN, subK)
-                 normal layout        (2048, 2048)          (2048 / subN, 2048 / subK, subN, subK)
-                                     T normal layout                    T native layout
-      It is recommended to use the rknn_B_normal_layout_to_native_layout interface for direct data conversion.
-      for RK3588/3576：
-      int4:
-      native layout: (N / 64, K / 32, 64, 32)
-              [K1N1,  K2N1,  ..., K32N1,
-               K1N2,  K2N2,  ..., K32N2,
-               ...
-               K1N64, K2N64, ..., K32N64,
-               K33N1, K34N1, ..., K64N1,
-               K33N2, K34N2, ..., K64N2,
-               ...
-               K(k-31)N64, K(k-30)N64, ..., KkN64,
-               K1N65, K2N65, ..., K32N65,
-               K1N66, K2N66, ..., K32N66,
-               ...
-               K(k-31)Nn, K(k-30)Nn, ..., KkNn]
-      int8:
-      native layout: (N / 32, K / 32, 32, 32)
-              [K1N1,  K2N1,  ..., K32N1,
-               K1N2,  K2N2,  ..., K32N2,
-               ...
-               K1N32, K2N32, ..., K32N32,
-               K33N1, K34N1, ..., K64N1,
-               K33N2, K34N2, ..., K64N2,
-               ...
-               K(k-31)N32, K(k-30)N32, ..., KkN32,
-               K1N33, K2N33, ..., K32N33,
-               K1N34, K2N34, ..., K32N34,
-               ...
-               K(k-31)Nn, K(k-30)Nn, ..., KkNn]
-      float16:
       native layout: (N / 16, K / 32, 16, 32)
               [K1N1,  K2N1,  ..., K32N1,
                K1N2,  K2N2,  ..., K32N2,
@@ -428,77 +134,13 @@ int rknn_matmul_create_dynamic_shape(rknn_matmul_ctx* ctx, rknn_matmul_info* inf
                M2N1, M2N2, ..., M2Nn,
                ...
                MmN1, MmN2, ..., MmNn]
-      native layout: (N / 4, M, 4)
+      perf layout: (N / 4, M, 4)
               [N1M1, N2M1, ..., N4M1,
                N5M2, N6M2, ..., N8M2,
                ...
                N(n-3)Mm, N(n-2)Mm, ..., NnMm]
-      for RK3588：
-      int4:
-      native layout: (N / 8, M, 8)
-              [N1M1, N2M1, ..., N8M1,
-               N9M2, N10M2, ..., N16M2,
-               ...
-               N(n-7)Mm, N(n-6)Mm, ..., NnMm]
  */
 int rknn_matmul_set_io_mem(rknn_matmul_ctx ctx, rknn_tensor_mem* mem, rknn_matmul_tensor_attr* attr);
-
-/*  rknn_matmul_set_core_mask
-
-    set rknn core mask.(only support RK3588 in current)
-
-    RKNN_NPU_CORE_AUTO: auto mode, default value
-    RKNN_NPU_CORE_0: core 0 mode
-    RKNN_NPU_CORE_1: core 1 mode
-    RKNN_NPU_CORE_2: core 2 mode
-    RKNN_NPU_CORE_0_1: combine core 0/1 mode
-    RKNN_NPU_CORE_0_1_2: combine core 0/1/2 mode
-
-    input:
-        rknn_matmul_ctx context     the handle of context.
-        rknn_core_mask core_mask    the core mask.
-    return:
-        int                         error code.
-*/
-int rknn_matmul_set_core_mask(rknn_matmul_ctx context, rknn_core_mask core_mask);
-
-/*  rknn_matmul_set_quant_params
-
-    set quant params.(only support matmul type RKNN_INT8_MM_INT8_TO_INT8, RKNN_INT8_MM_INT8_TO_INT32)
-
-    input:
-        rknn_matmul_ctx context     the handle of context.
-        rknn_quant_params params    quant params.
-    return:
-        int                         error code.
-*/
-int rknn_matmul_set_quant_params(rknn_matmul_ctx context, rknn_quant_params* params);
-
-/*  rknn_matmul_get_quant_params
-
-    get per channel quant params.(only support matmul type RKNN_INT8_MM_INT8_TO_INT32)
-
-    input:
-        rknn_matmul_ctx context     the handle of context.
-        rknn_quant_params params    quant params.
-        float scale    get scale for user.
-    return:
-        int                         error code.
-*/
-int rknn_matmul_get_quant_params(rknn_matmul_ctx ctx, rknn_quant_params* params, float* scale);
-
-/*  rknn_matmul_set_dynamic_shape
-
-    set the matmul input/output shape. matmul will run under current input shape after rknn_matmul_set_dynamic_shape,
-    only support M dynamicly now.
-
-    input:
-        rknn_matmul_ctx ctx         the handle of context.
-        rknn_matmul_shape* shape    the M,K,N shape of matmul currently
-    return:
-        int                         error code.
-*/
-int rknn_matmul_set_dynamic_shape(rknn_matmul_ctx ctx, rknn_matmul_shape* shape);
 
 /*  rknn_matmul_run
 
@@ -521,21 +163,6 @@ int rknn_matmul_run(rknn_matmul_ctx ctx);
         int                         error code.
  */
 int rknn_matmul_destroy(rknn_matmul_ctx ctx);
-
-/*  rknn_B_normal_layout_to_native_layout
-
-    change the B normal layout buffer to native layout buffer
-
-    params:
-        void* B_input               B normal layout buffer.
-        void* B_output              B native layout buffer.
-        int   K                     K
-        int   N                     N
-        rknn_matmul_info info       matmul info
-    return:
-        int                         error code.
- */
-int rknn_B_normal_layout_to_native_layout(void* B_input, void* B_output, int K, int N, rknn_matmul_info* info);
 
 #ifdef __cplusplus
 } // extern "C"
